cba0884d8ecb1470c1257eb2d44930388d67d60d
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2022-04-08 17:38:36 +0200

    add logger

--- api_service.py
+++ api_service.py
@@ -1,3 +1,5 @@
+import logging
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -17,15 +19,16 @@ class ApiService:
     def search(self) -> AppInformation:
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, 'html.parser')
-
-        name = soup.find('h1').text
-        version = soup.find_all('div', attrs={'class': APP_VERSION_CSS_CLASS})[0].text
-        download_count = soup.find_all('span', attrs={'class': APP_NUMBER_OF_DOWNLOADS_CSS_CLASS})[0].text
-        release_date = soup.find_all('div', attrs={'class': APP_RELEASE_DATE_CSS_CLASS})[0].text.strip("()")
-        description = soup.find_all('p', attrs={'class': APP_DESCRIPTION_CSS_CLASS})[0].text
-
-        return AppInformation(name=name,
-                              version=version,
-                              download=download_count,
-                              release_date=release_date,
-                              description=description)
+        try:
+            name = soup.find('h1').text
+            version = soup.find_all('div', attrs={'class': APP_VERSION_CSS_CLASS})[0].text
+            download_count = soup.find_all('span', attrs={'class': APP_NUMBER_OF_DOWNLOADS_CSS_CLASS})[0].text
+            release_date = soup.find_all('div', attrs={'class': APP_RELEASE_DATE_CSS_CLASS})[0].text.strip("()")
+            description = soup.find_all('p', attrs={'class': APP_DESCRIPTION_CSS_CLASS})[0].text
+            return AppInformation(name=name,
+                                  version=version,
+                                  download=download_count,
+                                  release_date=release_date,
+                                  description=description)
+        except IndexError as e:
+            logging.error(f"Couldn't find the html element with class name {e}")
--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
+from typing import Any, Union
+
+from flask import Flask, render_template, request, redirect, url_for, logging
 from flask_wtf import FlaskForm
+from werkzeug import Response
 from wtforms import SubmitField, URLField
 from wtforms.validators import DataRequired, URL
 
 from api_service import ApiService
+from logger import config
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('default_settings')
-    return app
+def create_app() -> Any:
+    flask_app = Flask(__name__)
+    flask_app.config.from_object('default_settings')
+    return flask_app
 
 
+logging.config.dictConfig(config)
 app = create_app()
 
 
@@ -21,13 +26,13 @@ class SearchForm(FlaskForm):
 
 
 @app.route('/search', methods=['POST'])
-def search():
+def search() -> Union[Response, str]:
     api_service = ApiService(request.form.get('url'))
     return render_template("detail.html", data=api_service.search())
 
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def home() -> Union[Response, str]:
     form = SearchForm(request.form)
     if form.validate_on_submit():
         return redirect(url_for('search'))
